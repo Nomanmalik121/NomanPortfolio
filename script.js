48012c3946e58189371f7e2a81b6df26dae31749
Nomanmalik121 <158442701+Nomanmalik121@users.noreply.github.com>
2024-09-29 18:59:47 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('#main'),
+  smooth: true
+});
+gsap.from(".section__pic-container",{
+  opacity: "0",
+  y:300,
+  duration: 1,
+  delay: 1,
+})
+gsap.from(".section__text",{
+  opacity: "0",
+  x:300,
+  duration: 1,
+  delay: 1,
+})
+gsap.from("#desktop-nav",{
+  opacity: "0",
+  y:-100,
+  duration: 1,
+  delay: 1,
+})
+
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
